6fbb331ba6215754b1e1b116d2615fa20cadce1d
shub-10 <116427176+shub-10@users.noreply.github.com>
2026-04-09 17:43:48 +0530

    feat: create interviewContext hook using context api

--- client/src/Context/interviewContext.jsx
+++ client/src/Context/interviewContext.jsx
@@ -5,10 +5,14 @@ const InterviewContext = createContext();
 export const InterviewProvider = ({children})=>{
     const [selectedRole, setSelectedRole] = useState('')
     const [interviewCategory, setInterviewCategory] = useState('Role');
-    const [difficultyLevel, setDifficultyLevel] = useState('easy');
-
+    const [difficultyLevel, setDifficultyLevel] = useState('');
+    const [selectedRound, setSelectedRound] = useState('')
+    const [jobTitle, setJobTitle] = useState('');
+    const [interviewType, setInterviewType] = useState('');
     return (
-      <InterviewContext.Provider value={{selectedRole, setSelectedRole, difficultyLevel, setDifficultyLevel, interviewCategory, setInterviewCategory}}>
+      <InterviewContext.Provider value={{selectedRole, setSelectedRole, difficultyLevel, setDifficultyLevel, interviewCategory, setInterviewCategory, selectedRound, setSelectedRound,
+        jobTitle, setJobTitle,interviewType, setInterviewType
+      }}>
         {children}
       </InterviewContext.Provider>
     )
