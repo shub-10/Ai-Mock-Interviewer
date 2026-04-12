8f111f52c45a32fd9a0a46135fa6c5b7b4a74f94
shub-10 <116427176+shub-10@users.noreply.github.com>
2026-04-12 16:09:32 +0530

    fix: User get logout automatically on token expiry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,16 @@ import {Login} from './pages/Login'
 import {Signup} from './pages/Signup'
 import {Interview} from './pages/Interview'
 import {InterviewRoom} from './pages/InterviewRoom'
-
+import {Report} from './pages/Report'
+import {jwtDecode} from 'jwt-decode'
 export const App = () => {
+
+  const token = localStorage.getItem('Aitoken')
+
+  if(token){
+    const decoded = jwtDecode(token)
+    if(decoded.exp*1000 < Date.now()) localStorage.removeItem('Aitoken')
+  }
   return (
     <div className="w-full min-h-screen m-0 p-0">
       <Navbar></Navbar>
@@ -20,6 +28,7 @@ export const App = () => {
           <Route path='/signup' element={<Signup/>} />
           <Route path='/job-interviews' element={<Interview/>} />
           <Route path='/:slug/interview' element={<InterviewRoom/>}/>
+          <Route path='/reports' element={<Report/>}/>
         </Routes>
       </main>
       <Footer></Footer>
