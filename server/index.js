41562e33b538566cce3ae4372283e9a166c1744c
shub-10 <116427176+shub-10@users.noreply.github.com>
2026-04-13 07:26:53 +0530

    Add heatlth check route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ app.use(cors({
   origin: process.env.FRONTEND
 }))
 
-
+app.use('/health', (req, res)=>{
+  res.send("Backend is running..")
+})
 app.use('/api/v2/auth', AuthRouter);
 app.use('/api/v2/interview', interviewRouter);
 const PORT = process.env.PORT
